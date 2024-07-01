684a113602adeba568323683c41725cedd9c8f4d
David Hernandez <davidjorgeagramonte@gmail.com>
2024-07-01 18:42:57 -0400

    Creando temporizador

---
+++ script.js
@@ -0,0 +1,10 @@
+const timeDisplay = document.getElementById("time-display");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const btnStart = document.getElementById("start");
+const btnPause = document.getElementById("pause");
+const btnReset = document.getElementById("reset");
+
+document.addEventListener('DOMContentLoaded',()=>{
+    
+})
